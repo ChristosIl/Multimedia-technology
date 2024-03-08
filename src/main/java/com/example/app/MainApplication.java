c4bcdce81aa9250fc9bff86628183fae7513b368
ChristosIl <chrisiliak1000@gmail.com>
2024-03-08 12:38:27 +0200

    name changes

--- src/main/java/com/example/app/MainApplication.java
+++ src/main/java/com/example/app/MainApplication.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import com.example.app.UserDataManager;
+
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // Initialize user list with admin if not already done
@@ -39,7 +39,7 @@ public class HelloApplication extends Application {
         //CategoryManager categoryManager = CategoryManager.getInstance();
         //categoryManager.clearCategories();
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Library Management!");
         stage.setScene(scene);
--- src/main/java/com/example/app/MainController.java
+++ src/main/java/com/example/app/MainController.java
@@ -12,12 +12,11 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-public class HelloController {
+public class MainController {
     @FXML
     private AnchorPane rootPane;
     @FXML
